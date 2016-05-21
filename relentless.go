8fb2ebf64d499f444c901299eac397aea8ea1a18
joshadambell <joshadambell@me.com>
2016-05-21 17:01:12 -0600

    add email

--- relentless.go
+++ relentless.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
+	"strings"
 
 	"golang.org/x/net/context"
 
@@ -13,9 +17,10 @@ import (
 func main() {
 	mux := goji.NewMux()
 	mux.HandleFuncC(pat.Get("/hello/:name"), hello)
+	mux.HandleFuncC(pat.Post("/signup"), signupH)
 	http.Handle("/", http.FileServer(http.Dir("app")))
 
-	http.ListenAndServe("localhost:8000", mux)
+	http.ListenAndServe("localhost:8080", mux)
 }
 
 type signup struct {
@@ -31,4 +36,34 @@ type signupOptions struct {
 func hello(c context.Context, w http.ResponseWriter, r *http.Request) {
 	name := r.Form.Get("name")
 	fmt.Fprintf(w, "Hello, %s!", name)
+}
+
+func signupH(c context.Context, w http.ResponseWriter, r *http.Request) {
+	var s signup
+	d := json.NewDecoder(r.Body)
+	d.Decode(&s)
+	fmt.Println(s)
+
+	notifyWithEmail("New recruit", s.Email, "joshadambell@me.com")
+}
+
+func notifyWithEmail(subject, body string, emails ...string) {
+	hostname, _ := os.Hostname()
+	if strings.HasSuffix(hostname, "local") {
+		fmt.Println(subject)
+		fmt.Println(body)
+		return
+	}
+	emailAddresses := strings.Join(emails, ",")
+
+	cmd := exec.Command("mail", "-s", subject, "-a", "Content-Type: text/html", "-t", emailAddresses)
+	cmd.Stdin = strings.NewReader(body)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	if string(out) != "" {
+		fmt.Printf("email response: %q\n", string(out))
+	}
+	return
 }
